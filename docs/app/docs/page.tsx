25bd1f2b5b42452f91e8fb369cd713cbe5f0f1bb
kane50613 <kane@yeecord.com>
2025-06-26 18:15:12 +0800

    feat(docs): add docs

---
+++ docs/app/docs/page.tsx
@@ -0,0 +1,63 @@
+import * as path from "node:path";
+import { createCompiler } from "@fumadocs/mdx-remote";
+import { executeMdxSync } from "@fumadocs/mdx-remote/client";
+import type { PageTree } from "fumadocs-core/server";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import {
+  DocsBody,
+  DocsDescription,
+  DocsPage,
+  DocsTitle,
+} from "fumadocs-ui/page";
+import { source } from "@/source";
+import type { Route } from "./+types/page";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+const compiler = createCompiler({
+  development: false,
+});
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const slugs = params["*"].split("/").filter((v) => v.length > 0);
+  const page = source.getPage(slugs);
+  if (!page) throw new Error("Not found");
+
+  const compiled = await compiler.compileFile({
+    path: path.resolve("content/docs", page.file.path),
+    value: page.data.content,
+  });
+
+  return {
+    page,
+    compiled: compiled.toString(),
+    tree: source.pageTree,
+  };
+}
+
+export default function Page(props: Route.ComponentProps) {
+  const { page, compiled, tree } = props.loaderData;
+  const { default: Mdx, toc } = executeMdxSync(compiled);
+
+  return (
+    <DocsLayout
+      nav={{
+        title: "React Router",
+      }}
+      tree={tree as PageTree.Root}
+    >
+      <DocsPage toc={toc}>
+        <DocsTitle>{page.data.title}</DocsTitle>
+        <DocsDescription>{page.data.description}</DocsDescription>
+        <DocsBody>
+          <Mdx components={defaultMdxComponents} />
+        </DocsBody>
+      </DocsPage>
+    </DocsLayout>
+  );
+}
---
+++ docs/app/docs/search.ts
@@ -0,0 +1,18 @@
+import { structure } from "fumadocs-core/mdx-plugins";
+import { createSearchAPI } from "fumadocs-core/search/server";
+import { source } from "@/source";
+import type { Route } from "./+types/search";
+
+const server = createSearchAPI("advanced", {
+  indexes: source.getPages().map((page) => ({
+    id: page.url,
+    url: page.url,
+    title: page.data.title ?? "",
+    description: page.data.description,
+    structuredData: structure(page.data.content),
+  })),
+});
+
+export async function loader({ request }: Route.LoaderArgs) {
+  return server.GET(request);
+}
