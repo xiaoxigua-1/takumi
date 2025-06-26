aa5193cf593933cea3e61f5c5573fc039a56aca9
kane50613 <kane@yeecord.com>
2025-06-26 19:41:04 +0800

    chore(docs): remove floating promise

--- docs/react-router.config.ts
+++ docs/react-router.config.ts
@@ -3,7 +3,7 @@ import { source } from "./app/source";
 
 export default {
   ssr: true,
-  async prerender({ getStaticPaths }) {
+  prerender({ getStaticPaths }) {
     return [...getStaticPaths(), ...source.getPages().map((page) => page.url)];
   },
 } satisfies Config;
