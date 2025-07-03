c48fad677004a487faab756512893ddd568aebd1
Kane Wang <kane@yeecord.com>
2025-07-03 18:22:41 +0800

    refactor(docs): load takumi packages using sandpack

--- docs/app/components/image-editor.tsx
+++ docs/app/components/image-editor.tsx
@@ -1,90 +1,98 @@
-import type { Editor as EditorType } from "@monaco-editor/react";
-import type { AnyNode } from "@takumi-rs/helpers";
-import { useCallback, useEffect, useState } from "react";
-import { ImageRender } from "./image-render";
+import {
+  FileTabs,
+  SandpackCodeEditor,
+  SandpackFileExplorer,
+  SandpackLayout,
+  SandpackPreview,
+  SandpackProvider,
+  SandpackStack,
+  useActiveCode,
+  useSandpack,
+} from "@codesandbox/sandpack-react";
+import { isbot } from "isbot";
+import { useEffect, useState } from "react";
+import index from "~/template/index.js?raw";
+import css from "~/template/styles.css?raw";
+import { Editor } from "@monaco-editor/react";
 
-const defaultCode = `import { container, percentage } from "@takumi-rs/helpers";
+async function getPackageVersion(name: string) {
+  const response = await fetch(`https://registry.npmjs.org/${name}`, {
+    headers: {
+      accept: "application/vnd.npm.install-v1+json",
+    },
+  });
 
-container({
-  background_color: 0xff0000,
-  width: percentage(100),
-  height: percentage(100),
-})`;
+  const json = (await response.json()) as {
+    "dist-tags": {
+      latest: string;
+    };
+  };
+
+  return json["dist-tags"].latest;
+}
 
 export function ImageEditor() {
-  const [Editor, setEditor] = useState<typeof EditorType>();
-  const [node, setNode] = useState<AnyNode>();
-  const [evaluatorUtils, setEvaluatorUtils] = useState<{
-    configureMonacoEditor: () => void;
-    safeEvaluate: (code: string) => AnyNode | null;
-  } | null>(null);
-
-  const handleEditorChange = useCallback(
-    (value: string | undefined) => {
-      if (!value || !evaluatorUtils) return;
-      const newNode = evaluatorUtils.safeEvaluate(value);
-      if (newNode) {
-        setNode(newNode);
-      }
-    },
-    [evaluatorUtils],
-  );
+  const [version, setVersion] = useState<string>();
 
   useEffect(() => {
-    if (evaluatorUtils) return;
+    if (!isbot()) void getPackageVersion("@takumi-rs/wasm").then(setVersion);
+  }, []);
 
-    // Dynamic import of code evaluator utilities and Monaco Editor
-    void import("../../lib/code-evaluator")
-      .then((utils) => {
-        setEvaluatorUtils(utils);
-        utils.configureMonacoEditor();
-
-        return import("@monaco-editor/react");
-      })
-      .then((module) => {
-        setEditor(() => module.default);
-        handleEditorChange(defaultCode);
-      });
-  }, [handleEditorChange, evaluatorUtils]);
+  // FIXME: maybe a fixed container to prevent CLS problem, or a button to start this editor?
+  if (!version) return <p>Loading version...</p>;
 
   return (
-    <div className="grid sm:grid-cols-2 rounded-xl overflow-hidden">
-      <div className="bg-[#1e1e1e] border-r flex flex-col aspect-video">
-        <div className="bg-[#2d2d30] px-4 py-2 border-b border-gray-600">
-          <div className="flex items-center gap-2">
-            <span className="ml-2 text-gray-300 text-sm font-medium">
-              render.js
-            </span>
-          </div>
-        </div>
-        <div className="flex-1">
-          {Editor ? (
-            <Editor
-              height="100%"
-              defaultLanguage="typescript"
-              defaultValue={defaultCode}
-              theme="vs-dark"
-              onChange={handleEditorChange}
-              options={{
-                minimap: { enabled: false },
-                scrollBeyondLastLine: false,
-                fontSize: 14,
-                fontFamily: "Monaco, 'Courier New', monospace",
-                lineNumbers: "on",
-                renderWhitespace: "selection",
-                automaticLayout: true,
-              }}
-            />
-          ) : (
-            <div className="flex items-center justify-center h-full text-gray-300">
-              Loading editor...
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="aspect-video grid place-items-center">
-        {node && <ImageRender node={node} />}
-      </div>
-    </div>
+    <SandpackProvider
+      customSetup={{
+        dependencies: {
+          "@takumi-rs/helpers": version,
+          "@takumi-rs/wasm": version,
+        },
+      }}
+      files={{
+        "index.js": index,
+        ".version": version,
+        "styles.css": css,
+      }}
+      template="vite"
+      theme="dark"
+    >
+      <SandpackLayout>
+        <MonacoEditor />
+        <SandpackPreview showRefreshButton={false} />
+      </SandpackLayout>
+    </SandpackProvider>
   );
+}
+
+function MonacoEditor() {
+  const { code, updateCode } = useActiveCode();
+  const { sandpack } = useSandpack();
+
+  return (
+    <SandpackStack style={{ margin: 0 }}>
+      <FileTabs />
+      <div style={{ flex: 1, paddingTop: 8, background: "#1e1e1e" }}>
+        <Editor
+          width="100%"
+          height="100%"
+          language={getLanguageFromPath(sandpack.activeFile)}
+          theme="vs-dark"
+          key={sandpack.activeFile}
+          defaultValue={code}
+          onChange={(value) => updateCode(value || "")}
+        />
+      </div>
+    </SandpackStack>
+  );
+}
+
+function getLanguageFromPath(path: string) {
+  switch (path.slice(path.lastIndexOf("."))) {
+    case ".js":
+      return "javascript";
+
+    default:
+      return "text";
+  }
 }
