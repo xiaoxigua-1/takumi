8db2f8dafa7e90f4fcea2f0c4fa3ca9889feba9e
Kane Wang <kane@yeecord.com>
2025-08-10 16:15:07 +0800

    Docs: fix font link

--- docs/app/template/shared/renderer.js
+++ docs/app/template/shared/renderer.js
@@ -6,15 +6,11 @@ await init(wasmUrl);
 
 export const renderer = new Renderer();
 
-renderer.loadFont(
-  await getFont("https://takumi.kane.tw/fonts/noto-sans-v39-latin-500.woff2"),
-);
-renderer.loadFont(
-  await getFont("https://takumi.kane.tw/fonts/noto-sans-v39-latin-700.woff2"),
-);
+renderer.loadFont(await getFont("noto-sans-v39-latin-500.woff2"));
+renderer.loadFont(await getFont("noto-sans-v39-latin-700.woff2"));
 
 async function getFont(file) {
-  const response = await fetch(`/fonts/${file}`);
+  const response = await fetch(`https://takumi.kane.tw/fonts/${file}`);
 
   if (!response.ok) {
     throw new Error(`Failed to load font: ${file}`);
