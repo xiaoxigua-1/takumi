ee52a3ba5a052e42abb24d1707c4c8d602fecbeb
Kane Wang <kane@yeecord.com>
2025-08-30 14:55:00 +0800

    Crate: fix `Sides` css parsing

--- takumi/src/layout/style/properties/sides.rs
+++ takumi/src/layout/style/properties/sides.rs
@@ -18,14 +18,14 @@ pub struct Sides<T: TS + Copy>(pub [T; 4]);
 #[derive(Debug, Clone, Deserialize, Serialize, TS, PartialEq)]
 #[serde(untagged)]
 pub enum SidesValue<T> {
-  /// Same value for all four sides
-  SingleValue(T),
-  /// Separate values for vertical and horizontal sides (vertical, horizontal)
-  AxisSidesArray(T, T),
-  /// Individual values for each side (top, right, bottom, left)
-  AllSides(T, T, T, T),
   /// CSS string representation
   Css(String),
+  /// Individual values for each side (top, right, bottom, left)
+  AllSides(T, T, T, T),
+  /// Separate values for vertical and horizontal sides (vertical, horizontal)
+  AxisSidesArray(T, T),
+  /// Same value for all four sides
+  SingleValue(T),
 }
 
 impl<T: TS + Copy + for<'i> FromCss<'i>> TryFrom<SidesValue<T>> for Sides<T> {
@@ -33,30 +33,48 @@ impl<T: TS + Copy + for<'i> FromCss<'i>> TryFrom<SidesValue<T>> for Sides<T> {
 
   fn try_from(value: SidesValue<T>) -> Result<Self, Self::Error> {
     match value {
-      SidesValue::SingleValue(value) => Ok(Sides([value; 4])),
-      SidesValue::AxisSidesArray(vertical, horizontal) => {
-        Ok(Sides([vertical, horizontal, vertical, horizontal]))
-      }
-      SidesValue::AllSides(top, right, bottom, left) => Ok(Sides([top, right, bottom, left])),
       SidesValue::Css(string) => {
         let mut input = ParserInput::new(&string);
         let mut parser = Parser::new(&mut input);
 
         // Parse between 1 and 4 values of T using FromCss
         let first = T::from_css(&mut parser).map_err(|e| e.to_string())?;
-        let second = parser.try_parse(T::from_css).ok();
-        let third = parser.try_parse(T::from_css).ok();
-        let fourth = parser.try_parse(T::from_css).ok();
 
-        let sides = match (second, third, fourth) {
-          (None, _, _) => Sides([first; 4]),
-          (Some(h), None, _) => Sides([first, h, first, h]),
-          (Some(r), Some(b), None) => Sides([first, r, b, r]),
-          (Some(r), Some(b), Some(l)) => Sides([first, r, b, l]),
+        // Collect all values by parsing until we can't parse more
+        let mut values = Vec::with_capacity(4);
+
+        values.push(first);
+
+        // Keep parsing values separated by whitespace
+        loop {
+          // Try to parse the next value
+          match parser.try_parse(T::from_css) {
+            Ok(next_value) => values.push(next_value),
+            Err(_) => break,
+          }
+
+          // Don't allow more than 4 values
+          if values.len() >= 4 {
+            break;
+          }
+        }
+
+        // Now create the sides based on how many values we got
+        let sides = match values.len() {
+          1 => Sides([values[0]; 4]),
+          2 => Sides([values[0], values[1], values[0], values[1]]),
+          3 => Sides([values[0], values[1], values[2], values[1]]),
+          4 => Sides([values[0], values[1], values[2], values[3]]),
+          _ => unreachable!(),
         };
 
         Ok(sides)
       }
+      SidesValue::AllSides(top, right, bottom, left) => Ok(Sides([top, right, bottom, left])),
+      SidesValue::AxisSidesArray(vertical, horizontal) => {
+        Ok(Sides([vertical, horizontal, vertical, horizontal]))
+      }
+      SidesValue::SingleValue(value) => Ok(Sides([value; 4])),
     }
   }
 }
