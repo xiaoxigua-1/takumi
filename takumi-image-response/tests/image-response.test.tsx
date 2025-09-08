a01ac2a48cc9ec3f92424f75fad4ee44f9e3f9e1
Kane Wang <kane@yeecord.com>
2025-09-08 20:33:54 +0800

    Template: publish `@takumi-rs/template`

--- takumi-image-response/tests/image-response.test.tsx
+++ takumi-image-response/tests/image-response.test.tsx
@@ -10,4 +10,15 @@ describe("ImageResponse", () => {
 
     expect(await response.arrayBuffer()).toBeDefined();
   });
+
+  test("should set content-type", async () => {
+    const response = new ImageResponse(<div>Hello</div>, {
+      width: 100,
+      height: 100,
+      format: "png",
+    });
+
+    expect(response.headers.get("content-type")).toBe("image/png");
+    expect(await response.arrayBuffer()).toBeDefined();
+  });
 });
