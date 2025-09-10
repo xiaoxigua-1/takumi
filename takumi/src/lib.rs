bb4b0bf8ac8c05820844680f02a265f5fd1a3030
Kane Wang <kane@yeecord.com>
2025-09-10 13:45:15 +0800

    Docs: updates supported style properties

--- takumi/src/lib.rs
+++ takumi/src/lib.rs
@@ -69,7 +69,7 @@
 //!
 //! - [taffy](https://github.com/DioxusLabs/taffy) for the layout system.
 //! - [image](https://github.com/image-rs/image) for the image processing.
-//! - [cosmic-text](https://github.com/kornelski/cosmic-text) for the text rendering.
+//! - [parley](https://github.com/linebender/parley) for the text layout.
 //! - [woff2-patched](https://github.com/zimond/woff2-rs) for the font processing.
 //! - [ts-rs](https://github.com/AlephAlpha/ts-rs) for the type-safe serialization.
 //! - [resvg](https://github.com/linebender/resvg) for SVG parsing and rendering.
