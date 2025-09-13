5032cb3f5bd7b64a7d85c78d8e8504362f7e159e
Kane Wang <kane@yeecord.com>
2025-09-13 23:46:10 +0800

    Crate: fix test missing fonts

--- takumi/tests/test_utils.rs
+++ takumi/tests/test_utils.rs
@@ -1,13 +1,31 @@
 use std::{path::Path, sync::Arc};
 
 use image::{ColorType::Rgba8, load_from_memory, save_buffer};
-use parley::GenericFamily;
+use parley::{GenericFamily, fontique::FontInfoOverride};
 use takumi::{
   GlobalContext,
   layout::{Viewport, node::NodeKind},
   rendering::render,
   resources::image::ImageSource,
 };
+
+const TEST_FONTS: &[(&[u8], &str, GenericFamily)] = &[
+  (
+    include_bytes!("../../assets/fonts/geist/Geist[wght].woff2"),
+    "Geist",
+    GenericFamily::SansSerif,
+  ),
+  (
+    include_bytes!("../../assets/fonts/geist/GeistMono[wght].woff2"),
+    "Geist Mono",
+    GenericFamily::Monospace,
+  ),
+  (
+    include_bytes!("../../assets/fonts/noto-sans/NotoColorEmoji.ttf"),
+    "Noto Color Emoji",
+    GenericFamily::Emoji,
+  ),
+];
 
 fn create_test_context() -> GlobalContext {
   let context = GlobalContext::default();
@@ -21,14 +39,19 @@ fn create_test_context() -> GlobalContext {
     )),
   );
 
-  context
-    .font_context
-    .load_and_store(
-      include_bytes!("../../assets/fonts/noto-sans/NotoColorEmoji.ttf"),
-      None,
-      Some(GenericFamily::Emoji),
-    )
-    .unwrap();
+  for (font, name, generic) in TEST_FONTS {
+    context
+      .font_context
+      .load_and_store(
+        font,
+        Some(FontInfoOverride {
+          family_name: Some(name),
+          ..Default::default()
+        }),
+        Some(*generic),
+      )
+      .unwrap();
+  }
 
   context
 }
